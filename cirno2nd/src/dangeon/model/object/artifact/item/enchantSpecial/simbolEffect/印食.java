e0ae13f937c56437696ca1736303dd43a3b1cdeb
Koyanagi-Hiromu <koyanagi166@gmail.com>
2025-09-06 18:45:26 +0900

    食　発動率アップ

--- cirno2nd/src/dangeon/model/object/artifact/item/enchantSpecial/simbolEffect/印食.java
+++ cirno2nd/src/dangeon/model/object/artifact/item/enchantSpecial/simbolEffect/印食.java
@@ -1,18 +1,18 @@
 package dangeon.model.object.artifact.item.enchantSpecial.simbolEffect;
 
-import main.res.SE;
 import dangeon.controller.ThrowingItem;
 import dangeon.model.object.artifact.item.enchantSpecial.ENCHANT_SIMBOL;
 import dangeon.model.object.artifact.item.enchantSpecial.EnchantSpecial;
 import dangeon.model.object.creature.enemy.ルーミア;
 import dangeon.model.object.creature.player.Player;
+import main.res.SE;
 
 public class 印食 extends EnchantSpecial {
 	public static boolean effect(ThrowingItem throwingItem) {
 		if (!enchantSimbolAllCheck(CASE.DEF, ENCHANT_SIMBOL.食)) {
 			return false;
 		}
-		if (parcentCheck(30, ENCHANT_SIMBOL.食)) {
+		if (parcentCheck(50, ENCHANT_SIMBOL.食)) {
 			Player.me.setStandEffect(ルーミア.class, throwingItem.A.getDirection()
 					.getReverse());
 			SE.ATTACK_EAT.play();
